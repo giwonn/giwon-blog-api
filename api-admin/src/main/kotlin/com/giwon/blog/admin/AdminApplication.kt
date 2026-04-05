4d6df6c63649ed91096de86aaf8d1136416544b3
giwonn <l4279625@gmail.com>
2026-04-06 00:14:21 +0900

    fix: api-admin에서 @EnableScheduling 제거 - core 스케줄러는 api-blog에서만 실행

--- api-admin/src/main/kotlin/com/giwon/blog/admin/AdminApplication.kt
+++ api-admin/src/main/kotlin/com/giwon/blog/admin/AdminApplication.kt
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@EnableScheduling
 @EnableJpaRepositories(basePackages = ["com.giwon.blog"])
 @EntityScan(basePackages = ["com.giwon.blog"])
 @SpringBootApplication(scanBasePackages = ["com.giwon.blog"])
